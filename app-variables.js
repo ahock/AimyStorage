9b183d52f61deb02c4c8a2d61ea8b523e6f110ac
Andreas Hock <andreashock@bluewin.ch>
2021-04-17 14:23:44 +0000

    v0.1.9 - Digital Ocean deployment config added

--- app-variables.js
+++ app-variables.js
@@ -5,11 +5,15 @@ config.clientID = 'gb2306fpDknAcuITs3Mf5V73R0MsibOg';
 config.storageURL = 'http://storage.iod4all.com:8080';
 config.MongoURI = 'mongodb+srv://aimy:aimx4aimy@aimycluster-9in7r.mongodb.net/aimy?retryWrites=true&w=majority';
 //config.MongoURI = 'mongodb://ahock:taurus1ted@ds135069.mlab.com:35069/aimy';
-config.BindIP = '10.0.0.251';
+// Cloud9 IP
+//config.BindIP = '10.0.0.251';
+// AWS EC2 instance IP
 //config.BindIP = '172.31.14.135';
+// Digital Ocean IP
+config.BindIP = '138.68.102.77';
 config.apiVersion = '0.0.1';
-config.version = '0.1.8';
-config.comment = 'Mongo parameter keepAlive is set to active';
+config.version = '0.1.9';
+config.comment = 'Digital Ocean deployment config added';
 config.default_stuff =  ['red','green','blue','apple','yellow','orange','politics'];
 config.web.port = process.env.WEB_PORT || 9980;
 
